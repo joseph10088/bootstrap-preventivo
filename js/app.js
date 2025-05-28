5776a818c30445b70617da1cb9ec796a3fa79e63
joseph <giuseppemanna1998@icloud.com>
2025-05-28 19:41:37 +0200

    add file javascript recupero id form + id output

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// id form
+const form = document.getElementById('formUtente')
+
+// id output
+const risult = document.getElementById('risult')
+console.log(risult)
+
+form.addEventListener('submit', function(event){
+    event.preventDefault() // privato del comportamento di default
+
+})
